31c1edb8047c0015a6f7a81d5b1e3f31c1812fcd
jakub <jakub@kubax220.lan>
2020-09-26 19:27:47 +0200

    pinout update

--- blackbox.ino
+++ blackbox.ino
@@ -1,3 +1,28 @@
+/*
+ * HW: ESP32 DO it DEV KIT 1
+ * LCD 5110, OLED 1306 
+ * BMP280, GPS receiver
+ * rotary encoder
+ * 
+ * pinout:
+ * LCD 5110 / GPIO
+ * RST   D23
+ * CE    D1/txd
+ * Dout  D19
+ * Din   D18
+ * CLK   D5
+ * BCKL  D2
+ * 
+ * Rotary encoder
+ * left  D17  
+ * right D4
+ * buttn D15
+ * 
+ * I2C: OLED,BME280
+ * SDA  D21
+ * SCL  D22
+*/
+
 void setup() {
   // put your setup code here, to run once:
 
