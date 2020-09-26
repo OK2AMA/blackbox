f4eac7b300b20d03d5534ac9932dceecfae0b741
jakub <jakub@kubax220.lan>
2020-09-26 19:52:18 +0200

    first text on display

--- blackbox.ino
+++ blackbox.ino
@@ -23,12 +23,39 @@
  * SCL  D22
 */
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
 
+
+SSD1306Wire display(0x3c, SDA, SCL);   // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  display.init();
+
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+}
+
+void drawFontFaceDemo() {
+  // Font Demo1
+  // create more fonts at http://oleddisplay.squix.ch/
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 0, "Hello world");
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 10, "Hello world");
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 26, "Hello world");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  drawFontFaceDemo();
+  // write the buffer to the display
+  display.display();
+  while ( true ) {
+    delay(10);
+  }
 }
