d34cfe64037e9967e83b9beb5d5955e7d2937a25
jakub <jakub@kubax220.lan>
2020-09-26 23:44:15 +0200

    bmp280 is working

--- blackbox.ino
+++ blackbox.ino
@@ -1,42 +1,51 @@
 /*
- * HW: ESP32 DO it DEV KIT 1
- * LCD 5110, OLED 1306 
- * BMP280, GPS receiver
- * rotary encoder
- * 
- * pinout:
- * LCD 5110 / GPIO
- * RST   D23
- * CE    D1/txd
- * Dout  D19
- * Din   D18
- * CLK   D5
- * BCKL  D2
- * 
- * Rotary encoder
- * left  D17  
- * right D4
- * buttn D15
- * 
- * I2C: OLED,BME280
- * SDA  D21
- * SCL  D22
+   HW: ESP32 DO it DEV KIT 1
+   LCD 5110, OLED 1306
+   BMP280, GPS receiver
+   rotary encoder
+
+   pinout:
+   LCD 5110 / GPIO
+   RST   D23
+   CE    D1/txd
+   Dout  D19
+   Din   D18
+   CLK   D5
+   BCKL  D2
+
+   Rotary encoder
+   left  D17
+   right D4
+   buttn D15
+
+   I2C: OLED,BME280
+   SDA  D21
+   SCL  D22
 */
 
 #include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
-
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
 
 SSD1306Wire display(0x3c, SDA, SCL);   // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h
+Adafruit_BMP280 bmp; // I2C
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
   display.init();
-
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
+
+  bmp.begin(0x76);
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 void drawFontFaceDemo() {
@@ -56,6 +65,19 @@ void loop() {
   // write the buffer to the display
   display.display();
   while ( true ) {
-    delay(10);
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println();
+    delay(2000);
   }
 }
