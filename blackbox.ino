64f1e1e206cc2f72d8b5936771cf0152f051d4b8
jakub <jakub@kubax220.lan>
2020-09-28 15:38:29 +0200

    GPS works

--- blackbox.ino
+++ blackbox.ino
@@ -29,14 +29,18 @@
 #include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
-
+#include <TinyGPS.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
 Adafruit_PCD8544 displayLCD = Adafruit_PCD8544(5, 18, 19, 32, 23);
 SSD1306Wire display(0x3c, SDA, SCL);   // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h
 Adafruit_BMP280 bmp; // I2C
+TinyGPS gps;
+HardwareSerial hwSerial_2(2);
 
+#define RX2 16
+#define TX2 17
 
 void setup() {
   Serial.begin(115200);
@@ -47,16 +51,22 @@ void setup() {
 
   displayLCD.begin();
   displayLCD.setRotation(2);
-  displayLCD.setContrast(50);
+  displayLCD.setContrast(55);
   displayLCD.display(); // show splashscreen
   displayLCD.clearDisplay();   // clears the screen and buffer
-  displayLCD.drawPixel(10, 10, BLACK);
+  displayLCD.drawPixel(0, 0, BLACK);
   displayLCD.display();
   displayLCD.setTextSize(1);
   displayLCD.setTextColor(BLACK);
-  displayLCD.setCursor(20,20);
+  displayLCD.setCursor(0, 0);
   displayLCD.println("Hello, world!");
+  displayLCD.clearDisplay();
   displayLCD.display();
+
+  pinMode(2, OUTPUT);    // backlight
+  digitalWrite(2, HIGH); // sets the digital pin 13 on
+
+  hwSerial_2.begin(9600, SERIAL_8N1, RX2, TX2);
 
   bmp.begin(0x76);
   /* Default settings from datasheet. */
@@ -84,21 +94,13 @@ void drawFontFaceDemo() {
 }
 
 void loop() {
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
   drawFontFaceDemo();
   // write the buffer to the display
   while ( true ) {
-  
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-    Serial.println(" m");
 
     Serial.println();
 
@@ -106,11 +108,74 @@ void loop() {
     display.drawString(0, 0, "Teplota");
     display.drawString(45, 0, String(bmp.readTemperature()) + " *C");
     display.drawString(0, 15, "Tlak");
-    display.drawString(45, 15, String(bmp.readPressure()/100) + " hPa");
+    display.drawString(45, 15, String(bmp.readPressure() / 100) + " hPa");
     display.display();
 
-   delay(2000);
-   display.clear();
+    delay(1);
+
+    // For one second we parse GPS data and report some key values
+    for (unsigned long start = millis(); millis() - start < 1000;)
+    {
+      while (hwSerial_2.available())
+      {
+        char c = hwSerial_2.read();
+        Serial.print(c);
+
+        if (gps.encode(c)) // Did a new valid sentekince come in?
+          newData = true;
+      }
+    }
+    if (newData)
+    {
+      float flat, flon;
+      unsigned long age;
+      gps.f_get_position(&flat, &flon, &age);
+      Serial.print("LAT=");
+      Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+      Serial.print(" LON=");
+      Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+      Serial.print(" SAT=");
+      Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+      Serial.print(" PREC=");
+      Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+
+      displayLCD.setCursor(0, 0);
+      displayLCD.println("GPS receiver:");
+     // displayLCD.setCursor(0, 9);
+      displayLCD.print("La:");
+      displayLCD.println(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 5);
+      //displayLCD.setCursor(0, 18);
+      displayLCD.print(" Lo:");
+      displayLCD.println(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 5);
+     // displayLCD.setCursor(0, 27);
+      displayLCD.print(" SAT=");
+      displayLCD.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+      displayLCD.print(" ALT=");
+      displayLCD.print(gps.f_altitude() == TinyGPS::GPS_INVALID_F_ALTITUDE ? 0 : gps.f_altitude());
+
+      displayLCD.display();
+      displayLCD.clearDisplay();
+
+    }
+    else
+    {
+      displayLCD.setCursor(20, 20);
+      displayLCD.println("No GPS fix");
+      displayLCD.display();
+      displayLCD.clearDisplay();
+    }
+
+    gps.stats(&chars, &sentences, &failed);
+    Serial.print(" CHARS=");
+    Serial.print(chars);
+    Serial.print(" SENTENCES=");
+    Serial.print(sentences);
+    Serial.print(" CSUM ERR=");
+    Serial.println(failed);
+    if (chars == 0)
+      Serial.println("** No characters received from GPS: check wiring **");
+
+    display.clear();
 
   }
 }
